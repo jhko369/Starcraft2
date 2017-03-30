90d42f9126039d18f966695d7cc71e1b8d93c0b0
jhko369 <jhko369@hanyang.ac.kr>
2017-03-30 17:33:03 +0900

    Update SWstudio_activity.swift

--- SWstudio_activity.swift
+++ SWstudio_activity.swift
@@ -32,6 +32,40 @@ for i in 0...list2.count-2 {
 }
 
 print(ret2)
+//음수의 개수 구하기
+var a = [1,-5,8,7,-3,9],count = 0
+func minus() -> (Int) {
+    for i in 0..<a.count {
+        if (a[i] < 0) {
+            count += 1
+        }
+    }
+    return count
+}
+//피보나치 12번째 값 구하기
+var a1=0,a2=1,a3=0
+func fi() -> (Int) {
+    for i in 0...10 {
+        a3=a1+a2
+        a1=a2
+        a2=a3
+    }
+    return a3
+}
+//배열에서 3번째로 작은 수 구하기
+func min3 () -> (Int) {
+    var temp = 0
+    for i in 0..<a.count {
+        for j in 0..<i {
+            if(a[i]<a[j]){
+                temp = a[i]
+                a[i] = a[j]
+                a[j] = temp
+            }
+        }
+    }
+    return a[2]
+}
 
 
 
